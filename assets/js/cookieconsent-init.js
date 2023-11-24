0f95879741f525d4316e87ee90517c269f5f027f
Libo Su <l.su@alcides.tech>
2023-11-24 23:09:50 +0100

    initial commit

---
+++ assets/js/cookieconsent-init.js
@@ -0,0 +1,195 @@
+// obtain plugin
+var cc = initCookieConsent();
+
+// run plugin with your configuration
+cc.run({
+    //current_lang: 'de',
+    //auto_language: "document",
+    current_lang: document.documentElement.getAttribute('lang'),
+    autoclear_cookies: true,                   // default: false
+    page_scripts: true,                        // default: false
+
+    // mode: 'opt-in'                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
+    // delay: 0,                               // default: 0
+    // auto_language: null                     // default: null; could also be 'browser' or 'document'
+    // autorun: true,                          // default: true
+    // force_consent: false,                   // default: false
+    // hide_from_bots: false,                  // default: false
+    // remove_cookie_tables: false             // default: false
+    // cookie_name: 'cc_cookie',               // default: 'cc_cookie'
+    // cookie_expiration: 182,                 // default: 182 (days)
+    // cookie_necessary_only_expiration: 182   // default: disabled
+    // cookie_domain: location.hostname,       // default: current domain
+    // cookie_path: '/',                       // default: root
+    // cookie_same_site: 'Lax',                // default: 'Lax'
+    // use_rfc_cookie: false,                  // default: false
+    // revision: 0,                            // default: 0
+
+    onFirstAction: function (user_preferences, cookie) {
+        // callback triggered only once
+    },
+
+    onAccept: function (cookie) {
+        // ...
+    },
+
+    onChange: function (cookie, changed_preferences) {
+        // ...
+    },
+
+    languages: {
+        'en': {
+            consent_modal: {
+                title: 'Cookie Settings',
+                description: 'This website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
+                primary_btn: {
+                    text: 'Accept all',
+                    role: 'accept_all'              // 'accept_selected' or 'accept_all'
+                },
+                secondary_btn: {
+                    text: 'Reject all',
+                    role: 'accept_necessary'        // 'settings' or 'accept_necessary'
+                }
+            },
+            settings_modal: {
+                title: 'Cookie preferences',
+                save_settings_btn: 'Save settings',
+                accept_all_btn: 'Accept all',
+                reject_all_btn: 'Reject all',
+                close_btn_label: 'Close',
+                cookie_table_headers: [
+                    { col1: 'Name' },
+                    { col2: 'Domain' },
+                    { col3: 'Expiration' },
+                    { col4: 'Description' }
+                ],
+                blocks: [
+                    {
+                        title: 'Cookie usage 📢',
+                        description: 'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="/en/privacy-policy" class="cc-link">privacy policy</a> and <a href="/en/imprint" class="cc-link">imprint</a>.'
+                    }, {
+                        title: 'Strictly necessary cookies',
+                        description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly',
+                        toggle: {
+                            value: 'necessary',
+                            enabled: true,
+                            readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
+                        }
+                    }, {
+                        title: 'Performance and Analytics cookies',
+                        description: 'These cookies allow the website to remember the choices you have made in the past',
+                        toggle: {
+                            value: 'analytics',     // your cookie category
+                            enabled: false,
+                            readonly: false
+                        },
+                        cookie_table: [             // list of all expected cookies
+                            {
+                                col1: '^_ga',       // match all cookies starting with "_ga"
+                                col2: 'google.com',
+                                col3: '2 years',
+                                col4: 'description ...',
+                                is_regex: true
+                            },
+                            {
+                                col1: '_gid',
+                                col2: 'google.com',
+                                col3: '1 day',
+                                col4: 'description ...',
+                            }
+                        ]
+                    }, {
+                        title: 'Advertisement and Targeting cookies',
+                        description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you',
+                        toggle: {
+                            value: 'targeting',
+                            enabled: false,
+                            readonly: false
+                        }
+                    }, {
+                        title: 'More information',
+                        description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="/en/contact">contact us</a>.',
+                    }
+                ]
+            }
+        },
+        'de': {
+            consent_modal:{
+                title: 'Cookie-Einstellungen',
+                description: 'Diese Website verwendet essentielle Cookies, um ihren ordnungsgemäßen Betrieb zu gewährleisten, und Tracking-Cookies, um zu verstehen, wie Sie mit ihr interagieren. Letztere werden erst nach Zustimmung gesetzt. <button type="button" data-cc="c-settings" class="cc-link">Lass mich aussuchen</button>',
+                primary_btn: {
+                    text: 'Alle akzeptiren',
+                    role: 'accept_all',
+                },
+                secondary_btn: {
+                    text: 'Alle ablehnen',
+                    role: 'accept_necessary',
+                },
+            },
+            settings_modal:{
+                title: 'Cookie-Einstellungen',
+                save_settings_btn: 'Einstellungen speichern',
+                accept_all_btn: 'Alle akzeptiren',
+                reject_all_btn: 'Alle ablehnen',
+                close_btn_label: 'Close',
+                cookie_table_headers: [
+                    { col1: 'Name' },
+                    { col2: 'Domain' },
+                    { col3: 'Expiration' },
+                    { col4: 'Description' }
+                ],
+            blocks: [
+                {
+                    title: 'Cookie-Einstellungen 📢',
+                    description: 'Wir verwenden Cookies, um die Grundfunktionen der Website sicherzustellen und Ihr Online-Erlebnis zu verbessern. Sie können für jede Kategorie wählen, ob Sie sich jederzeit an- oder abmelden möchten. Für weitere Einzelheiten zu Cookies und anderen sensiblen Daten lesen Sie bitte die vollständige <a href="privacy-policy" class="cc-link">Datenschutzerklärung</a> und <a href="imprint" class="cc-link">Impressum</a>.'
+                }, {
+                    title: 'Unbedingt notwendige Cookies',
+                    description: 'Diese Cookies sind für das ordnungsgemäße Funktionieren meiner Website unerlässlich. Ohne diese Cookies würde die Website nicht richtig funktionieren',
+                    toggle: {
+                        value: 'Notwendig',
+                        enabled: true,
+                        readonly: true          // cookie categories with readonly=true are all treated as "necessary cookies"
+                    }
+                }, {
+                    title: 'Leistungs- und Analyse-Cookies',
+                    description: 'Diese Cookies ermöglichen es der Website, sich an die von Ihnen in der Vergangenheit getroffenen Entscheidungen zu erinnern',
+                    toggle: {
+                        value: 'Analytik',     // your cookie category
+                        enabled: false,
+                        readonly: false
+                    },
+                    cookie_table: [             // list of all expected cookies
+                            {
+                                col1: '^_ga',       // match all cookies starting with "_ga"
+                                col2: 'google.com',
+                                col3: '2 Jahre',
+                                col4: 'description ...',
+                                is_regex: true
+                            },
+                            {
+                                col1: '_gid',
+                                col2: 'google.com',
+                                col3: '1 tag',
+                                col4: 'description ...',
+                            }
+                        ]
+                }, {
+                    title: 'Werbung und Targeting-Cookies',
+                    description: 'Diese Cookies sammeln Informationen darüber, wie Sie die Website nutzen, welche Seiten Sie besucht und welche Links Sie angeklickt haben. Alle Daten sind anonymisiert und können nicht verwendet werden, um Sie zu identifizieren',
+                    toggle: {
+                        value: 'Targeting',
+                        enabled: false,
+                        readonly: false
+                    }
+                }, {
+                    title: 'Mehr Informationen',
+                    description: 'Wenn Sie Fragen zu unserer Cookie-Politik und Ihren Wahlmöglichkeiten haben, <a class="cc-link" href="contact">kontaktieren Sie uns bitte</a>.',
+                }
+            ]
+            }
+        }
+    }
+});
+
+// Set theme to alcides
+document.body.classList.toggle('c_alcides');
